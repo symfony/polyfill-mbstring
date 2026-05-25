1b6ebba07f7695a6e1c71ecd8bc11c873731e6fb
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-05-25 08:33:33 +0200

    Improve type declarations fo some mbstring polyfill

--- bootstrap.php
+++ bootstrap.php
@@ -133,26 +133,32 @@ if (!function_exists('mb_str_split')) {
 }
 
 if (!function_exists('mb_str_pad')) {
+    /** @return string|false */
     function mb_str_pad(string $string, int $length, string $pad_string = ' ', int $pad_type = STR_PAD_RIGHT, ?string $encoding = null) { return p\Mbstring::mb_str_pad($string, $length, $pad_string, $pad_type, $encoding); }
 }
 
 if (!function_exists('mb_ucfirst')) {
+    /** @return string|false */
     function mb_ucfirst(string $string, ?string $encoding = null) { return p\Mbstring::mb_ucfirst($string, $encoding); }
 }
 
 if (!function_exists('mb_lcfirst')) {
+    /** @return string|false */
     function mb_lcfirst(string $string, ?string $encoding = null) { return p\Mbstring::mb_lcfirst($string, $encoding); }
 }
 
 if (!function_exists('mb_trim')) {
+    /** @return string|false */
     function mb_trim(string $string, ?string $characters = null, ?string $encoding = null) { return p\Mbstring::mb_trim($string, $characters, $encoding); }
 }
 
 if (!function_exists('mb_ltrim')) {
+    /** @return string|false */
     function mb_ltrim(string $string, ?string $characters = null, ?string $encoding = null) { return p\Mbstring::mb_ltrim($string, $characters, $encoding); }
 }
 
 if (!function_exists('mb_rtrim')) {
+    /** @return string|false */
     function mb_rtrim(string $string, ?string $characters = null, ?string $encoding = null) { return p\Mbstring::mb_rtrim($string, $characters, $encoding); }
 }
 
